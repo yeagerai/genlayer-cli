c7eb09e9b1d0046cf2b00b7d32b96893f9d5e837
Cristiam Da Silva <cristiam86@gmail.com>
2024-04-19 01:10:11 +0200

    feat: setup commands tests with jest

--- .mocharc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  require: ["ts-node/register"],
-  extension: ["ts"],
-  spec: "tests/**/*.ts",
-  watchFiles: ["src/**/*.ts"],
-  recursive: true,
-  nodeOptions: ["--loader=ts-node/esm"],
-};
---
+++ tests/commands/init.test.ts
@@ -0,0 +1,50 @@
+import {program} from "commander";
+
+import {initializeGeneralCommands} from "../../src/commands/general";
+import {getCommand} from "../utils";
+
+jest.mock("inquirer", () => ({
+  prompt: jest.fn().mockResolvedValue({confirmDelete: true}),
+}));
+const action = jest.fn();
+
+describe("init command", () => {
+  initializeGeneralCommands(program);
+  const initCommand = getCommand("init");
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("doesnt have required arguments nor options", async () => {
+    expect(() => program.parse(["node", "test", "init"])).not.toThrow();
+  });
+
+  test("option -n, --numValidators is accepted", async () => {
+    expect(() => program.parse(["node", "test", "init", "-n", "10"])).not.toThrow();
+    expect(() => program.parse(["node", "test", "init", "--numValidators", "10"])).not.toThrow();
+  });
+
+  test("random option is not accepted", async () => {
+    initCommand?.exitOverride();
+    expect(() => program.parse(["node", "test", "init", "-random"])).toThrow(
+      "error: unknown option '-random'",
+    );
+    expect(() => program.parse(["node", "test", "init", "--randomOption"])).toThrow(
+      "error: unknown option '--randomOption'",
+    );
+  });
+
+  test("action is called", async () => {
+    // Given
+    initCommand?.action(action);
+    // When
+    program.parse(["node", "test", "init"]);
+    // Then
+    expect(action).toHaveBeenCalledTimes(1);
+  });
+});
