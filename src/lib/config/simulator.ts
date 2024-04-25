ad59a43697dc05c260691e32e36c85d762b5fbd3
Cristiam Da Silva <cristiam86@gmail.com>
2024-04-25 16:52:43 +0200

    chore: increase timeout to wait for simulator to start

--- src/lib/config/simulator.ts
+++ src/lib/config/simulator.ts
@@ -3,9 +3,9 @@ export const DEFAULT_REPO_GH_URL = "git@github.com:yeagerai/genlayer-simulator.g
 export const DEFAULT_RUN_SIMULATOR_COMMAND = (simulatorLocation: string) => ({
   darwin: `osascript -e 'tell application "Terminal" to do script "cd ${simulatorLocation} && cp .env.example .env && docker compose build && docker compose up"'`,
   win32: `start cmd.exe /k "cd ${simulatorLocation} && xcopy .env.example .env && docker compose build && docker compose up"`,
-  linux: `x-terminal-emulator -e 'bash -c "cd ${simulatorLocation} && cp .env.example .env && docker compose build && docker compose up"'`,
+  linux: `x-terminal-emulator -e bash -c 'cd ${simulatorLocation} && cp .env.example .env && docker compose build && docker compose up; echo "Press enter to exit"; read'`,
 });
 export const AVAILABLE_PLATFORMS = ["darwin", "win32", "linux"] as const;
 export type RunningPlatform = (typeof AVAILABLE_PLATFORMS)[number];
 export const STARTING_TIMEOUT_WAIT_CYLCE = 2000;
-export const STARTING_TIMEOUT_ATTEMPTS = 60;
+export const STARTING_TIMEOUT_ATTEMPTS = 120;
