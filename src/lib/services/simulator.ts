c95f4f945109969594bdcab07c4b5d3541eed45b
Edinaldo Junior <epsjunior@icloud.com>
2024-11-12 08:15:37 -0300

    fix: changing linux command to fix display issue and fixing waiting simulator issue

--- src/lib/services/simulator.ts
+++ src/lib/services/simulator.ts
@@ -221,7 +221,7 @@ export class SimulatorService implements ISimulatorService {
       const response = await rpcClient.request({method: "ping", params: []});
 
       //Compatibility with current simulator version
-      if (response?.result?.status === "OK" || response?.result?.data?.status === "OK") {
+      if (response?.result === "OK" || response?.result?.status === "OK" || response?.result?.data?.status === "OK") {
         return { initialized: true };
       }
       if (retries > 0) {
