b391e78990be814bcee9be60c1c87078e3ce6544
Edinaldo Junior <epsjunior@icloud.com>
2024-12-02 22:38:16 -0300

    feat: new config command

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import {version} from "../package.json";
 import {CLI_DESCRIPTION} from "../src/lib/config/text";
 import { initializeGeneralCommands } from "../src/commands/general";
 import { initializeKeygenCommands } from "../src/commands/keygen";
+import { initializeConfigCommands } from "../src/commands/config";
 
 export function initializeCLI() {
   program.version(version).description(CLI_DESCRIPTION);
   initializeGeneralCommands(program);
   initializeKeygenCommands(program);
+  initializeConfigCommands(program);
   program.parse(process.argv);
 }
 
